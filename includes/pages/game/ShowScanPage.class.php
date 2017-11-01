4dd37b7322936162904346572f813f16377d6d36
mimikri <pocco_13@yahoo.de>
2017-11-01 20:18:25 +0100

    bug-fix by bata - also show destroy moon mission

--- includes/pages/game/ShowScanPage.class.php
+++ includes/pages/game/ShowScanPage.class.php
@@ -11,7 +11,7 @@ function Show()
 		global $USER;
 		$db = Database::get();		
 		   $Alarm = $db->selectSingle("SELECT COUNT(`fleet_target_owner`) as nr
-FROM uni".$USER['universe']."_fleets  WHERE `fleet_target_owner` = '". $USER['id'] ."' AND (`fleet_mission` = '1' OR `fleet_mission` = '2') AND `fleet_mess` ='0';");
+FROM uni".$USER['universe']."_fleets  WHERE `fleet_target_owner` = '". $USER['id'] ."' AND (`fleet_mission` = '1' OR `fleet_mission` = '2' OR `fleet_mission` = '9') AND `fleet_mess` ='0';");
           echo $Alarm['nr'];
 		  exit;
 }
